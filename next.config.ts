0639fbf23f91e21efde057fb4c82b8c7262fefa4
Sarthak Dwivedi <114822434+sarthak7772@users.noreply.github.com>
2025-10-11 10:37:52 +0530

    ok

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,11 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   reactStrictMode: true,
-// };
-
-// export default nextConfig;
-
-
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // enables static HTML export
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+export default nextConfig;
+
+
+
